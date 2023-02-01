c9cf481e9c657553fb7285991042d1a38eda729e
Sylvester <sylvester@socialbrothers.nl>
2023-02-01 09:08:08 +0100

    Update kitty & nvim colorthemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,14 @@
 ---@type LazyPluginSpec[]
 local M = {
-  -- catppuccin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = { flavour = "mocha" },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      require("catppuccin").load(opts.flavour)
+ {    
+    'EdenEast/nightfox.nvim',
+    lazy = false,
+    config = function()
+      require("nightfox").load()
+
+      vim.cmd("colorscheme duskfox")
     end,
-    priority = 1000,
-  },
+  }
 }
 
 return M
