4cce25cc565c965b4fb39f9adbdc7975f55d8f88
Sylvester <hello@slvstr.dev>
2024-04-26 16:28:23 +0200

    Update wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,28 +4,24 @@ local config = wezterm.config_builder()
 -- Font settings
 config.font = wezterm.font "CaskaydiaCove Nerd Font"
 config.font_size = 16
-config.line_height = 1.4
+config.line_height = 1.2
 config.command_palette_font_size = 16
 
 -- Color scheme
 config.color_scheme = "Catppuccin Mocha"
 
 -- Window appearance
-config.macos_window_background_blur = 40
-config.window_background_opacity = 0.6
 config.window_decorations = "RESIZE"
+
+-- Tab bar appearance
+config.use_fancy_tab_bar = false
+config.colors = {
+  tab_bar = {
+    background = "rgb(30, 30, 46)",
+  }
+}
 
 -- Key bindings
 config.keys = require("keys")
-
--- Plugin configuration
-wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(config, {
-  position = "top",
-  indicator = {
-    leader = {
-      enabled = false,
-    },
-  },
-})
 
 return config
